87248a903d3c76d93dad599d05c503f7d2a91418
Ben <1989bjohnson@gmail.com>
2018-09-20 00:02:18 -0700

    added home and thanks route, moved questions data to json, added styling

---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+        <p>Click the 'Next' button below to begin the questionnaire!</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/thanks.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Thanks extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Thank you!</h1>
+        <p>We appreciate you response!</p>
+      </div>
+    );
+  }
+}
+
+export default Thanks;
